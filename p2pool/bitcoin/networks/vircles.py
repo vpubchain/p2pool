809833da5c64d1ccf154b41a3914481864e4b3ac
cekongsb <806138244@qq.com>
2020-04-07 11:38:48 +0800

    fork v1

--- p2pool/bitcoin/networks/vircles.py
+++ p2pool/bitcoin/networks/vircles.py
@@ -7,22 +7,19 @@ from .. import data, helper
 from p2pool.util import pack
 
 
-P2P_PREFIX = 'f9beb4d9'.decode('hex')
-P2P_PORT = 9069
-ADDRESS_VERSION = 63
-RPC_PORT = 9370
-RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            (yield helper.check_genesis_block(bitcoind, '0000066e6810ff0642cc34fe5fc3c66c3f39c9a0c713c079df427524994fc06a')) and
-            (yield bitcoind.rpc_getblockchaininfo())['chain'] != 'test'
-        ))
-SUBSIDY_FUNC = lambda height: 66.6*100000000 >> (height + 1)//525600
-POW_FUNC = data.hash256
-BLOCK_PERIOD = 60 # s
-SYMBOL = 'VCL'
-CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Vircles') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Vircles/') if platform.system() == 'Darwin' else os.path.expanduser('~/.vircles'), 'vircles.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'https://vircles.info/block/'
-ADDRESS_EXPLORER_URL_PREFIX = 'https://vircles.info/address/'
-TX_EXPLORER_URL_PREFIX = 'https://vircles.info/tx/'
-SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
-DUMB_SCRYPT_DIFF = 1
-DUST_THRESHOLD = 0.001e8
+P2P_PREFIX='dcececdc'.decode('hex') 
+P2P_PORT=9069
+ADDRESS_VERSION=63
+RPC_PORT=9370
+RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue('virclesaddress' in (yield bitcoind.rpc_help()) and not (yield bitcoind.rpc_getinfo())['testnet']))
+SUBSIDY_FUNC=lambda height: 66.6*100000000 >> (height + 1)//525600
+POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
+BLOCK_PERIOD=60
+SYMBOL='VCL'
+CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Vircles') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Vircles/') if platform.system() == 'Darwin' else os.path.expanduser('~/.vircles'), 'vircles.conf')
+BLOCK_EXPLORER_URL_PREFIX='http://sys.coinzone.info/chain/SysCoin/block/'
+ADDRESS_EXPLORER_URL_PREFIX='http://sys.coinzone.info/chain/SysCoin/address/'
+TX_EXPLORER_URL_PREFIX='http://sys.coinzone.info/chain/SysCoin/tx/'
+SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1)
+DUMB_SCRYPT_DIFF=2**16
+DUST_THRESHOLD=0.03e8
--- p2pool/networks/vircles.py
+++ p2pool/networks/vircles.py
@@ -1,29 +1,18 @@
 from p2pool.bitcoin import networks
 
-# CHAIN_LENGTH = number of shares back client keeps
-# REAL_CHAIN_LENGTH = maximum number of shares back client uses to compute payout
-# REAL_CHAIN_LENGTH must always be <= CHAIN_LENGTH
-# REAL_CHAIN_LENGTH must be changed in sync with all other clients
-# changes can be done by changing one, then the other
-
-PARENT = networks.nets['vircles']
-SHARE_PERIOD = 30 # seconds
-CHAIN_LENGTH = 24*60*60//10 # shares
-REAL_CHAIN_LENGTH = 24*60*60//10 # shares
-TARGET_LOOKBEHIND = 200 # shares
-SPREAD = 3 # blocks
-IDENTIFIER = 'fc70035c7a81bc6f'.decode('hex')
-PREFIX = '2472ef181efcd37b'.decode('hex')
-P2P_PORT = 9333
-MIN_TARGET = 0
-MAX_TARGET = 2**256//2**32 - 1
-PERSIST = True
-WORKER_PORT = 9332
-BOOTSTRAP_ADDRS = '47.105.157.227 118.190.201.80'.split(' ')
-ANNOUNCE_CHANNEL = '#p2pool-vcl'
-VERSION_CHECK = lambda v: None if 100000 <= v else 'vircles version too old. Upgrade to 0.11.2 or newer!' # not a bug. BIP65 support is ensured by SOFTFORKS_REQUIRED
-VERSION_WARNING = lambda v: None
-SOFTFORKS_REQUIRED = set(['bip65', 'csv', 'segwit'])
-MINIMUM_PROTOCOL_VERSION = 1600
-NEW_MINIMUM_PROTOCOL_VERSION = 1700
-SEGWIT_ACTIVATION_VERSION = 17
+PARENT=networks.nets['vircles']
+SHARE_PERIOD=10 # seconds target spacing
+CHAIN_LENGTH=12*60*60//10 # shares
+REAL_CHAIN_LENGTH=12*60*60//10 # shares
+TARGET_LOOKBEHIND=10 # shares coinbase maturity
+SPREAD=20 # blocks
+IDENTIFIER='24acdfeb0874eb2e'.decode('hex')
+PREFIX='fc8eba5cacdfe446'.decode('hex')
+P2P_PORT=8993
+MIN_TARGET=0
+MAX_TARGET=2**256//2**20 - 1
+PERSIST=True
+WORKER_PORT=8994
+BOOTSTRAP_ADDRS='118.190.201.80'.split(' ')
+ANNOUNCE_CHANNEL='#p2pool-alt'
+VERSION_CHECK=lambda v: True
