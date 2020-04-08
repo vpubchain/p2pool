f91998df0ed0135d412cf108b2b3832aeb7f8d9c
cekongsb <806138244@qq.com>
2020-04-08 16:56:38 +0800

    commit 2020-4-8

--- p2pool/bitcoin/networks/vircles.py
+++ p2pool/bitcoin/networks/vircles.py
@@ -11,9 +11,14 @@ P2P_PREFIX='dcececdc'.decode('hex')
 P2P_PORT=9069
 ADDRESS_VERSION=63
 RPC_PORT=9370
-RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue('virclesaddress' in (yield bitcoind.rpc_help()) and not (yield bitcoind.rpc_getinfo())['testnet']))
-SUBSIDY_FUNC=lambda height: 66.6*100000000 >> (height + 1)//525600
+# RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue('virclesaddress' in (yield bitcoind.rpc_help()) and not (yield bitcoind.rpc_getinfo())['testnet']))
+RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            (yield helper.check_genesis_block(bitcoind, '0000066e6810ff0642cc34fe5fc3c66c3f39c9a0c713c079df427524994fc06a')) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        ))
+SUBSIDY_FUNC = lambda height: 66.6*100000000 >> (height + 1)//525600
 POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
+# POW_FUNC = data.hash256
 BLOCK_PERIOD=60
 SYMBOL='VCL'
 CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Vircles') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Vircles/') if platform.system() == 'Darwin' else os.path.expanduser('~/.vircles'), 'vircles.conf')
